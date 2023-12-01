40118b28b669b64a584b0004a608c930da9dcd9c
Kiril Ivanov <kiril.ivanov91@gmail.com>
2023-12-02 00:39:00 +0200

    feature: site logo
    Add site logo in Header's Nav
    Reorder static files structure
    
    Closes #23

--- src/app/components/Header.rs
+++ src/app/components/Header.rs
@@ -11,8 +11,12 @@ pub fn Header(cx: Scope) -> Element {
         header{
             class: "flex sticky w-full top-0 h-24 justify-between bg-neutral-800 z-20",
             div {
-                class: "ml-0 order-first"
-                //TODO Convert to LOGO component
+                class: "ml-0 h-24 order-first",
+                img {
+                    class: "w-24 h-24 p-4",
+                    src: "/assets/image/dioxy_logo_v0.1.svg",
+                    alt: "Dioxycomp Logo"
+                }
             },
             Nav {},
         },
